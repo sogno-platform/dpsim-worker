a94094a369a40c56b4d5a5406f20e0f113313d0a
Richard Marston <rmarston@eonerc.rwth-aachen.de>
2022-09-19 13:54:04 +0200

    Use the load profile data if it has been sent
    Signed-off-by: Richard Marston <rmarston@eonerc.rwth-aachen.de>

--- image/receive.py
+++ image/receive.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import requests
 from glob import glob
-import pika, sys, os, json
+import pika, sys, os, json, traceback
 from villas.dataprocessing.readtools import *
 from villas.dataprocessing.timeseries import *
 import villas.dataprocessing.validationtools as validationtools
@@ -10,33 +10,54 @@ import zipfile
 import logging
 import base64
 
-def download_grid_data(name, url):
-    with open(name, 'wb') as out_file:
-        content = requests.get(url, stream=True).content
-        out_file.write(content)
-
 #
 # Start executing the dpsim simulation
 #
 # files - the names of the files containing the model
 #
 def run_dpsim(dpsim_config):
-    files            = dpsim_config["files"]
-    logging.info("Starting dpsim with model files: %s", str(files))
+    logging.info("Requested simulation config: %s", dpsim_config)
+
+    model_files      = dpsim_config["model_files"]
     results_file_id  = dpsim_config["results_file_id"]
     name             = 'CIGRE_MV'
     reader           = dpsimpy.CIMReader(name)
-    system           = reader.loadCIM(50, files, dpsimpy.Domain.SP, dpsimpy.PhaseType.Single, dpsimpy.GeneratorType.PVNode)
+    system           = reader.loadCIM(50, model_files, dpsimpy.Domain.SP,
+                                      dpsimpy.PhaseType.Single, dpsimpy.GeneratorType.PVNode)
     system
+    logging.info("Starting dpsim with model files: %s", str(model_files))
+    logging.info("Starting dpsim with load profile files: %s", str(model_files))
 
     sim = dpsimpy.RealTimeSimulation(name)
     sim.set_system(system)
 
-    logging.info("Requested simulation config: %s", dpsim_config)
+    # map from CSV to simulation names
+    assignList = {
+        'LOAD-H-1': 'Load_H_1',
+        'LOAD-H-3': 'Load_H_3',
+        'LOAD-H-4': 'Load_H_4',
+        'LOAD-H-5': 'Load_H_5',
+        'LOAD-H-6': 'Load_H_6',
+        'LOAD-H-8': 'Load_H_8',
+        'LOAD-H-10': 'Load_H_10',
+        'LOAD-H-11': 'Load_H_11',
+        'LOAD-H-12': 'Load_H_12',
+        'LOAD-H-14': 'Load_H_14',
+        'LOAD-I-1': 'Load_I_1',
+        'LOAD-I-3': 'Load_I_3',
+        'LOAD-I-7': 'Load_I_7',
+        'LOAD-I-9': 'Load_I_9',
+        'LOAD-I-10': 'Load_I_10',
+        'LOAD-I-12': 'Load_I_12',
+        'LOAD-I-13': 'Load_I_13',
+        'LOAD-I-14': 'Load_I_14'
+    }
 
-    # TODO: Add CSV reading
-    #csvreader = dpsimpy.CSVReader(name, csv_files, assignList, dpsimpy.LogLevel.info)
-    #csvreader.assignLoadProfile(system, 0, 1, 300, dpsimpy.CSVReaderMode.MANUAL, dpsimpy.CSVReaderFormat.SECONDS)
+    if dpsim_config["load_profile_files"] != "":
+        csvreader = dpsimpy.CSVReader(name, "/etc/config/loadprofile/",
+                                      assignList, dpsimpy.LogLevel.info)
+        csvreader.assignLoadProfile(system, 0, dpsim_config['timestep'], dpsim_config['finaltime'],
+                                    dpsimpy.CSVReaderMode.MANUAL, dpsimpy.CSVReaderFormat.SECONDS)
 
     # instantiate logger
     logger = dpsimpy.Logger(name)
@@ -81,32 +102,47 @@ def run_dpsim(dpsim_config):
     for node in system.nodes:
         sim.log_idobj_attribute(node.name(), 'v')
 
-    sim.run(1)
-
-    logging.info("Simulation complete, uploading results")
-
-    path = 'logs/'
-    logName = 'CIGRE_MV'
-    dpsim_result_file = path + logName + '.csv'
-    with open(dpsim_result_file, 'rb') as f:
-        result_bytes = f.read().decode('utf-8').encode('ascii')
-        base64_bytes = base64.b64encode(result_bytes)
-        base64_ascii = base64_bytes.decode('ascii')
-        r = requests.put("http://sogno-file-service:8080/api/files/"+results_file_id, files={'file': '{"ready":"true", "content":"' + base64_ascii + '"}'})
     try:
-        string_content = r.content.decode('utf8')
+        error = ""
+
+        logging.info("Simulation starting")
+
+        sim.run(1)
+
+        logging.info("Simulation complete, uploading results")
+
+        path = 'logs/'
+        logName = 'CIGRE_MV'
+        dpsim_result_file = path + logName + '.csv'
+        with open(dpsim_result_file, 'rb') as f:
+            result_bytes = f.read().decode('utf-8').encode('ascii')
+            base64_bytes = base64.b64encode(result_bytes)
+            base64_ascii = base64_bytes.decode('ascii')
+            r = requests.put("http://sogno-file-service:8080/api/files/"+results_file_id,
+                             files={'file': '{"ready":"true", "content":"' + base64_ascii + '"}'})
+        string_content = r.content.decode('utf8')        # possible source of UnicodeDecodeError
+        json_content = json.loads(string_content)        # possible source of JSONDecodeError
+        file_id      = json_content["data"]["fileID"]
+        # Generate exceptions:
+        # other_value  = "\xc3\x28".decode('utf8')       # generate UnicodeDecodeError
+        # other_value  = json.loads('{"hhh":"jdk}')      # generate json.JSONDecodeError
+        # other_value  = json_content["ohno"]            # generate Exception
+        logging.info('Uploaded results to fileID: %s', file_id)
+
     except UnicodeDecodeError:
         logging.error("Failed to decode data as utf8 bytes %s", r.content)
-        return
-
-    try:
-        json_content = json.loads(string_content)
-        file_id      = json_content["data"]["fileID"]
-    except JSONDecodeError:
+        error = "Failed to decode data as utf8 bytes.\n" + traceback.format_exc()
+    except json.JSONDecodeError:
         logging.error("Failed to decode data as json %s", string_content)
-        return
+        error = "Failed to decode data as json.\n" + traceback.format_exc()
+    except Exception as e:
+        logging.error("Caught exception e:\n%s" + traceback.format_exc())
+        error = "Caught exception e:\n" + traceback.format_exc()
 
-    logging.info('Uploaded results to fileID: %s', file_id)
+    if error != "":
+        r = requests.put("http://sogno-file-service:8080/api/files/"+results_file_id,
+                         files={'file': '{"ready":"false", "error": ' + error + '"}'})
+    return
 
 def get_url_list(model):
     type = model.get('type', 'url')
@@ -124,7 +160,7 @@ def get_url_list(model):
     return urls
 
 def download_file(url, headers, directory):
-    logging.info('Downloading model: %s', url)
+    logging.info('Downloading file: %s', url)
 
     r = requests.get(url,
         headers=headers)
@@ -153,7 +189,7 @@ def unzip_files(directory):
     logging.info("Unzipped files: " + str(arr))
     return arr
 
-def download_model(model, directory):
+def download_zipped_data(model, directory):
     url_list = get_url_list(model)
 
     token = model.get('token')
@@ -170,6 +206,7 @@ def download_model(model, directory):
 
 def callback(ch, method, properties, body):
     logging.info("Received a message: " + str(body))
+    ch.close()
     received = body.decode("utf-8")
     data = None
 
@@ -180,16 +217,27 @@ def callback(ch, method, properties, body):
 
     dpsim_config = {}
 
-    if data != None and data['model'] != None:
-        download_model(data['model'], '/etc/config/model')
-        logging.info("PARAMETERS: %s", data["parameters"])
-        dpsim_config["files"] = unzip_files('/etc/config/model/')
-        dpsim_config["results_file_id"] = data["parameters"]["results_file"]
-        dpsim_config["domain"] = data["parameters"]["domain"]
-        dpsim_config["solver"] = data["parameters"]["solver"]
-        dpsim_config["timestep"] = data["parameters"]["timestep"]
-        dpsim_config["finaltime"] = data["parameters"]["finaltime"]
-        logging.info("The status will be upated in file with id: %s", dpsim_config["results_file_id"])
+    try:
+        if data != None and 'load_profile' in data and data['load_profile'] != "":
+            download_zipped_data(data['load_profile'], '/etc/config/loadprofile')
+            dpsim_config["load_profile_files"] = unzip_files('/etc/config/loadprofile/')
+        else:
+            logging.info("No load_profile in %s", dpsim_config)
+            dpsim_config["load_profile_files"] = ""
+        if data != None and 'model' in data:
+            download_zipped_data(data['model'], '/etc/config/model')
+            logging.info("PARAMETERS: %s", data["parameters"])
+            dpsim_config["model_files"] = unzip_files('/etc/config/model/')
+            dpsim_config["results_file_id"] = data["parameters"]["results_file"]
+            dpsim_config["domain"] = data["parameters"]["domain"]
+            dpsim_config["solver"] = data["parameters"]["solver"]
+            dpsim_config["timestep"] = data["parameters"]["timestep"]
+            dpsim_config["finaltime"] = data["parameters"]["finaltime"]
+            logging.info("The status will be upated in file with id: %s", dpsim_config["results_file_id"])
+        else:
+            logging.info("No model in", dpsim_config)
+    except KeyError as e:
+        logging.error("Error trying to read from message:" + str(e))
 
     run_dpsim(dpsim_config)
 
@@ -215,11 +263,10 @@ def open_rabbitmq_connection():
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='dpsim-worker-queue')
 
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue='dpsim-worker-queue', on_message_callback=callback, auto_ack=True)
 
-    logging.info(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
 def main():
